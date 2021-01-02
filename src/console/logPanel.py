9dc54f926f3c73104aa781c7dfe9201678062afb
Mike Frank <michael.patrick.frank@gmail.com>
2021-01-02 13:09:31 -0700

    Um.

---
+++ src/console/logPanel.py
@@ -0,0 +1,29 @@
+#|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#|					 TOP OF FILE:	 console/logPanel.py
+#|vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+"""
+	FILE NAME:		console/logPanel.py				 [Python module source file]
+		
+	MODULE NAME:	console.logPanel
+	IN PACKAGE:		console
+	FULL PATH:		$GIT_ROOT/GLaDOS/src/console/logPanel.py
+	MASTER REPO:	https://github.com/mikepfrank/GLaDOS.git
+	SYSTEM NAME:	GLaDOS (General Lifeform and Domicile Operating System)
+	APP NAME:		GLaDOS.server (Main GLaDOS server application)
+	SW COMPONENT:	GLaDOS.console (GLaDOS System Console)
+
+
+	MODULE DESCRIPTION:
+	===================
+
+		This module implements the main system console of the GLaDOS server.
+		This runs on the tty where the server process is started.  A human 
+		operator can use this console to monitor the system status, and enter
+		maintenance commands.
+"""
+#|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#| End of module documentation string.
+#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# logPanel.py
