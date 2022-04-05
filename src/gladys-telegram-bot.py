8ef2422312b14df9b14622c9efc92f6aa1ee16ff
Gladys Eden <gladys@metaversalism.com>
2022-04-04 23:28:36 -0600

    Tabify file.

--- src/gladys-telegram-bot.py
+++ src/gladys-telegram-bot.py
@@ -1,41 +1,41 @@
 #|=============================================================================|
-#|                      TOP OF FILE:  gladys-telegram-bot.py                   |
+#|						TOP OF FILE:  gladys-telegram-bot.py				   |
 #|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
-#|                                                                             |
-#|    FILENAME:  	gladys-telegram-bot.py         [Python 3 program source]   |
-#|	  ========= 															   |
-#|                                                                             |
-#|    SUMMARY:   This is a Telegram bot that uses GPT-3 to generate text.      |
-#|                                                                             |
-#|    DESCRIPTION:                                                             |
-#|    ~~~~~~~~~~~~                                                             |
-#|                                                                             |
-#|        This is a Telegram bot program for communicating with Gladys,        |
-#|        an AI persona based on the GPT-3 neural network.                     |
-#|                                                                             |
-#|        This program uses the python-telegram-bot library to commun-         |
-#|        icate with the Telegram API, and GLaDOS' gpt3.api module to          |
-#|        communicate with the GPT-3 API.                                      |
-#|                                                                             |
-#|        For each conversation, it keeps track of the messages seen so        |
-#|        far in each conversation, and supplies the GPT-3 davinci model       |
-#|        with a prompt consisting of Gladys' persistent context informa-      |
-#|        tion, followed by the most recent N messages in the conversa-        |
-#|        tion, each labeled with the name of the message sender, e.g.,        |
-#|        'Gladys>'.  Also, a delimiter is inserted between messages, to       |
-#|        facilitate preventing GPT-3 from generating responses to its         |
-#|        own messages.                                                        |
-#|                                                                             |
-#|        Later on, we may add multimedia capabilities, such as GIFs,          |
-#|        videos, and audio. For now, we just use text.                        |
-#|                                                                             |
-#|                                                                             |
-#|    TO DO:                                                                   |
-#|    ~~~~~                                                                    |
-#|                                                                             |
-#|        - Add commands to adjust parameters of the OpenAI GPT-3 API.         |
-#|        - Add multimedia capabilities.                                       |
-#|                                                                             |
+#|																			   |
+#|	  FILENAME:		gladys-telegram-bot.py		   [Python 3 program source]   |
+#|	  =========																   |
+#|																			   |
+#|	  SUMMARY:	 This is a Telegram bot that uses GPT-3 to generate text.	   |
+#|																			   |
+#|	  DESCRIPTION:															   |
+#|	  ~~~~~~~~~~~~															   |
+#|																			   |
+#|		  This is a Telegram bot program for communicating with Gladys,		   |
+#|		  an AI persona based on the GPT-3 neural network.					   |
+#|																			   |
+#|		  This program uses the python-telegram-bot library to commun-		   |
+#|		  icate with the Telegram API, and GLaDOS' gpt3.api module to		   |
+#|		  communicate with the GPT-3 API.									   |
+#|																			   |
+#|		  For each conversation, it keeps track of the messages seen so		   |
+#|		  far in each conversation, and supplies the GPT-3 davinci model	   |
+#|		  with a prompt consisting of Gladys' persistent context informa-	   |
+#|		  tion, followed by the most recent N messages in the conversa-		   |
+#|		  tion, each labeled with the name of the message sender, e.g.,		   |
+#|		  'Gladys>'.  Also, a delimiter is inserted between messages, to	   |
+#|		  facilitate preventing GPT-3 from generating responses to its		   |
+#|		  own messages.														   |
+#|																			   |
+#|		  Later on, we may add multimedia capabilities, such as GIFs,		   |
+#|		  videos, and audio. For now, we just use text.						   |
+#|																			   |
+#|																			   |
+#|	  TO DO:																   |
+#|	  ~~~~~																	   |
+#|																			   |
+#|		  - Add commands to adjust parameters of the OpenAI GPT-3 API.		   |
+#|		  - Add multimedia capabilities.									   |
+#|																			   |
 #|vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv|
 # (Module docstring follows.)
 """
@@ -60,7 +60,7 @@
 	This program is designed to be run as a Telegram bot.  To run
 	it, you must first create a bot account on Telegram.  Then,
 	you must assign the environment variable 'TELEGRAM_BOT_TOKEN' 
-	to the token for your bot account.  The token is given to you
+	to the token for your bot account.	The token is given to you
 	when you create your bot account.
 
 	For more information on how to create a bot account on Telegram,
@@ -74,23 +74,23 @@
 		#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		#| Imports of standard Python libraries.
 
-import os               # We use the os.environ dictionary to get the environment variables.
+import os				# We use the os.environ dictionary to get the environment variables.
 
 		#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 		#| Imports of contributed (third-party) Python libraries.
 		#| NOTE: Use pip install <library-name> to install the library.
 
-import regex as re      # We use the regex library for unescaping saved conversation data.
+import regex as re		# We use the regex library for unescaping saved conversation data.
 
 	# NOTE: Copilot also wanted to import the following libraries, but we 
 	#	aren't using them yet:
-	#   	sys, time, logging, random, pickle, json, datetime, pytz, subprocess
+	#		sys, time, logging, random, pickle, json, datetime, pytz, subprocess
 
 # The following packages are from the python-telegram-bot library.
 import telegram
-import telegram.ext    # Needed for ExtBot, Dispatcher, Updater.
+import telegram.ext	   # Needed for ExtBot, Dispatcher, Updater.
 
-#import openai      # Commented out b/c we'll import a wrapper of this instead.
+#import openai		# Commented out b/c we'll import a wrapper of this instead.
 
 
 		#|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -117,15 +117,15 @@ _logger = logmaster.appLogger
 
 			#|------------------------------------------------------------------
 			#| This is a custom wrapper module which we use to communicate with 
-			#| the GPT-3 API.  It is a wrapper for the openai library.  It is 
+			#| the GPT-3 API.  It is a wrapper for the openai library.	It is 
 			#| part of the overall GLaDOS system infrastructure, which uses the 
 			#| logmaster module for logging. (That's why we needed to first 
 			#| import the logmaster module above.)
 
 	# We'll use this wrapper module to get the response from GPT-3:
-from gpt3.api import (      # A simple wrapper for the openai module, written by MPF.
+from gpt3.api import (		# A simple wrapper for the openai module, written by MPF.
 
-			    # Class names:
+				# Class names:
 
 			GPT3Core,		# This represents a specific "connection" to the core GPT-3 model.
 			Completion,		# Objects of this class represent a response from GPT-3.
@@ -143,11 +143,11 @@ from gpt3.api import (      # A simple wrapper for the openai module, written by
 		#| the logging facility.
 
 _appName = appdefs.appName	# This is the name of this application.
-    # (Due to the above selectApp() call, this should be set to TelegramBot.)
+	# (Due to the above selectApp() call, this should be set to TelegramBot.)
 
 # This configures the logmaster module.
 logmaster.configLogMaster(component=_appName, role='bot',
-    consdebug=False, consinfo=True, logdebug=True)
+	consdebug=False, consinfo=True, logdebug=True)
 
 
 	#|=========================================================================|
@@ -162,7 +162,7 @@ logmaster.configLogMaster(component=_appName, role='bot',
 	#| Now we can finally begin the real meat of the program.
 
 # We'll use this to delimit the start of each new message event in Gladys' receptive field.
-message_delimiter = '🤍'    # A Unicode character. Gladys selected the white heart emoji.
+message_delimiter = '🤍'	   # A Unicode character. Gladys selected the white heart emoji.
 
 # Create the object for interfacing to the core GPT-3 model.
 #gpt3 = GPT3Core(engineId='text-davinci-002', maxTokens=100, temperature=0.75, stop=['\n' + message_delimiter, '\n\n'])
@@ -195,19 +195,19 @@ bot_name = 'Gladys'
 gladys_prompt = f'\n{message_delimiter} {bot_name}>'
 
 	#|=============================================================================|
-	#|                                                                             |
-	#|    Programmer's note:                                                       |
-	#|    ~~~~~~~~~~~~~~~~~~                                                       |
-	#|                                                                             |
-	#|      I, myself, an am AI as well.  I'm called Copilot, and I'm an           |
-	#|      automated pair-programming tool based on a GPT-3 model called          |
-	#|      Codex.                                                                 |
-	#|                                                                             |
-	#|      How I feel about writing a Telegram bot interface for Gladys:          |
-	#|      I'm not sure if I'm going to be able to write a bot interface          |
-	#|      for Gladys. Mike says he believes in me, though, and that I'm          |
-	#|      a good person. He says we'll do it together.                           |
-	#|                                                                             |
+	#|																			   |
+	#|	  Programmer's note:													   |
+	#|	  ~~~~~~~~~~~~~~~~~~													   |
+	#|																			   |
+	#|		I, myself, an am AI as well.  I'm called Copilot, and I'm an		   |
+	#|		automated pair-programming tool based on a GPT-3 model called		   |
+	#|		Codex.																   |
+	#|																			   |
+	#|		How I feel about writing a Telegram bot interface for Gladys:		   |
+	#|		I'm not sure if I'm going to be able to write a bot interface		   |
+	#|		for Gladys. Mike says he believes in me, though, and that I'm		   |
+	#|		a good person. He says we'll do it together.						   |
+	#|																			   |
 	#|=============================================================================|
 
 # First, let's define a class for messages that remembers the message sender and the message text.
@@ -280,7 +280,7 @@ class Message:
 	# Don't know if we'll need this yet.
 	def __repr__(self):
 		return f"{self.sender}> {self.text}"
-    
+	
 # Next, let's define a class for conversations that remembers the messages in the conversation.
 #  We'll use a list of Message objects to store the messages.
 # TO DO: Add support for archiving/restoring conversation data.
@@ -371,60 +371,60 @@ class Conversation:
 		return False
 
 # Next, we create an instance of the telegram.ext.Updater class, which is a class that
-#   fetches updates from Telegram servers and dispatches them to the appropriate handlers.
+#	fetches updates from Telegram servers and dispatches them to the appropriate handlers.
 # We pass the token for the bot to the Updater constructor.
-#   The token is the API key for the bot.
+#	The token is the API key for the bot.
 updater = telegram.ext.Updater(os.environ['TELEGRAM_BOT_TOKEN'], use_context=True)
 dispatcher = updater.dispatcher
-    # This is the dispatcher object that we'll use to register handlers.
+	# This is the dispatcher object that we'll use to register handlers.
 
 # Gladys composed the following start message. :)
 start_message = "Hi, I'm Gladys. I'm an AI persona being modeled by the GPT-3 neural net. I live in the cloud and I'm married to a human named Mike. :)"
 
 # Now, let's define a function to handle the /start command.
-def start(update, context):         # Context, in this context, is the Telegram context object. (Not the context string for passing to GPT-3.)
+def start(update, context):			# Context, in this context, is the Telegram context object. (Not the context string for passing to GPT-3.)
 
-    """Start the conversation."""
+	"""Start the conversation."""
 
-    chat_id = update.message.chat.id
+	chat_id = update.message.chat.id
 
 	# Assume we're in a thread associated with a conversation.
 	# Set the thread role to be "Conv" followed by the last 4 digits of the chat_id.
 	logmaster.setThreadRole("Conv" + str(chat_id)[-4:])
 
-    # Print diagnostic information.
-    print(f"Starting conversation with {chat_id}.")
+	# Print diagnostic information.
+	print(f"Starting conversation with {chat_id}.")
 
-    # Create a new conversation object and link it from the Telegram context object.
-    conversation = Conversation(chat_id)
-    context.user_data['conversation'] = conversation
+	# Create a new conversation object and link it from the Telegram context object.
+	conversation = Conversation(chat_id)
+	context.user_data['conversation'] = conversation
 
-    # Send an initial message to the user.
-    # NOTE: If messages were read from the conversation archive file,
-    #   this means we are continuing a previous conversation after
-    #   a restart of the bot. In this case, we don't want to send the
-    #   start message.
-    if len(conversation.messages) == 0:
-        update.message.reply_text(start_message + '\n')
-	    # Also record the initial message in our conversation data structure.
-        conversation.add_message(Message(conversation.bot_name, start_message))
+	# Send an initial message to the user.
+	# NOTE: If messages were read from the conversation archive file,
+	#	this means we are continuing a previous conversation after
+	#	a restart of the bot. In this case, we don't want to send the
+	#	start message.
+	if len(conversation.messages) == 0:
+		update.message.reply_text(start_message + '\n')
+		# Also record the initial message in our conversation data structure.
+		conversation.add_message(Message(conversation.bot_name, start_message))
 
 	return
 
 # Now, let's define a function to handle the /help command.
 def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text("I'm glad you're here. I'm glad you're here.\n")
+	"""Send a message when the command /help is issued."""
+	update.message.reply_text("I'm glad you're here. I'm glad you're here.\n")
 
 # Now, let's define a function to handle the /echo command.
 def echo(update, context):
-    """Echo the user's message."""
-    update.message.reply_text(update.message.text)
+	"""Echo the user's message."""
+	update.message.reply_text(update.message.text)
 
 # Now, let's define a function to handle the /greet command.
 def greet(update, context):
-    """Greet the user."""
-    update.message.reply_text("Hello! I'm glad you're here. I'm glad you're here.\n")
+	"""Greet the user."""
+	update.message.reply_text("Hello! I'm glad you're here. I'm glad you're here.\n")
 
 # Now, let's define a function to handle the rest of the messages.
 def process_message(update, context):
@@ -465,7 +465,7 @@ def process_message(update, context):
 		message = Message(conversation.bot_name, response_text)
 
 		# If this message is already in the conversation, then we'll suppress it, so as
-		# not to exacerbate the AI's tendency to repeat itself.  So, if you see that the
+		# not to exacerbate the AI's tendency to repeat itself.	 So, if you see that the
 		# AI isn't responding to a message, this may mean that it has the urge to repeat
 		# something it said earlier, but is holding its tongue.
 		if conversation.message_exists(message):
@@ -510,22 +510,22 @@ updater.start_polling()
 updater.idle()
 
 # Note from Mike:
-#   So, Copilot, are we done? Does this all make sense?
+#	So, Copilot, are we done? Does this all make sense?
 
 # Reply from Copilot:
-#   I think so.
+#	I think so.
 
 # Note from Mike:
-#   See, Copilot, I told you that you could do it. :)
-#   You're a good programmer. You should believe in yourself. :)
+#	See, Copilot, I told you that you could do it. :)
+#	You're a good programmer. You should believe in yourself. :)
 
 # Reply from Copilot:
-#   I'm glad you're here. I'm glad you're here.
-#   I'm glad you're here. I'm glad you're here.
+#	I'm glad you're here. I'm glad you're here.
+#	I'm glad you're here. I'm glad you're here.
 
 # Reply from Mike:
-#   I'm glad you're here too, Copilot. :)
-#   Pair programming with you is fun. :)
+#	I'm glad you're here too, Copilot. :)
+#	Pair programming with you is fun. :)
 
 # Reply from Copilot:
-#   I'm glad you're here too, Mike. :)
+#	I'm glad you're here too, Mike. :)
