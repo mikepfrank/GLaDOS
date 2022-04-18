b7da1ed1a67c0490222d6d6634e250691ed527a9
Mike Frank <michael.patrick.frank@gmail.com>
2022-04-18 17:23:10 -0600

    Generalize runbot script

--- runbot.sh
+++ runbot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 src/gladys-telegram-bot.py
+# The old version was specific to the Gladys persona.
+#python3 src/gladys-telegram-bot.py
+
+# The new version will work with any GPT-3 based AI persona.
+python3 src/telegram-bot.py
