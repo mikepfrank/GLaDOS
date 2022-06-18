32cbe24c0053f6fbfc6f27724c2c21f902b06bb9
Mike Frank <michael.patrick.frank@gmail.com>
2022-06-18 15:55:31 -0600

    Add /forget command to AI doc
    Document the /forget command for the AI's benefit.

--- src/telegram-bot.py
+++ src/telegram-bot.py
@@ -247,6 +247,7 @@ def initializePersistentContext():
         PERSISTENT_DATA + \
         MESSAGE_DELIMITER + " ~~~ Available commands: ~~~\n" + \
         "  /remember <text> - Adds <text> to persistent context data.\n" + \
+        "  /forget <text>   - Removes <text> from persistent context data.\n" + \
         MESSAGE_DELIMITER + " ~~~ Recent Telegram messages: ~~~\n"
 
 # Go ahead and call it now.
