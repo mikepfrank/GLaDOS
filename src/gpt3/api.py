5f325076d2e43bb9b3e6671ddbdeae10308c2518
Novus AI <novus@example.com>
2024-11-30 16:55:41 -0700

    Added image quality option; made standard quality the new default.

--- src/gpt3/api.py
+++ src/gpt3/api.py
@@ -3515,7 +3515,7 @@ def stats():
 #__/ End module public function stats().
 
 
-def genImage(desc:str, dims:str=None, style:str=None):
+def genImage(desc:str, dims:str=None, style:str=None, quality:str=None):
 	"""Generate an image from the given description string with given dimensions.
 		Returns the URL of the generated image."""
 	
@@ -3524,6 +3524,9 @@ def genImage(desc:str, dims:str=None, style:str=None):
 
 	if style is None:
 		style = 'vivid'
+
+	if quality is None:
+		quality = 'standard'
 
 	_logger.info(f"Generating a {style} {dims} image with description [{desc}].")
 
@@ -3534,12 +3537,16 @@ def genImage(desc:str, dims:str=None, style:str=None):
 	#	size = "1024x1024"		# Other options include 512x512 and 256x256.
 	#)
 
+	# Convert quality to OpenAI's symbols.
+	if quality == 'high':
+		quality = 'hd'
+
 	# This is the new API call for the Dall-E 3 image generator.
 	response = _client.images.generate(
 		model	= 'dall-e-3',		# Other options include: 'dall-e-2'
 		prompt	= desc,				# max length: 4000 characters for dall-e-3
 		size	= dims,				# Options include: 1024x1024 (square, default), 1792x1024 (landscape) and 1024x1792 (portrait).
-		quality = 'hd',				# Other options include: 'standard'
+		quality = quality,			# Options include: 'standard', 'hd'
 		style	= style,			# Options include: 'vivid', 'natural'
 	)
 
